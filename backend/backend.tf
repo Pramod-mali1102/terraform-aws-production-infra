cd5d9b444748536ce2ece4338902765a015a08b5
Pramod <PRAMOD12345MALI@GMAIL.COM>
2026-06-10 19:38:57 +0530

    Update backend.tf

--- backend/backend.tf
+++ backend/backend.tf
@@ -1,1 +1,15 @@
+terraform {
 
+  backend "s3" {
+
+    bucket         = "YOUR-TERRAFORM-STATE-BUCKET"
+
+    key            = "production/terraform.tfstate"
+
+    region         = "ap-south-1"
+
+    dynamodb_table = "terraform-lock"
+
+    encrypt        = true
+  }
+}
